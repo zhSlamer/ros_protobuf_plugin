9068223dfea59b16212b3f2b386d60ccd6c8f907
zh <1658890297@qq.com>
2025-07-09 21:24:04 +0800

    delete

--- build_for_self/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/zh/backend/ros_protobuf_plugin/ros_protobuf_msg/build_for_self/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build_for_self/sample_protos/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zh/backend/ros_protobuf_plugin/ros_protobuf_msg/sample_protos
-# Build directory: /home/zh/backend/ros_protobuf_plugin/ros_protobuf_msg/build_for_self/sample_protos
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
